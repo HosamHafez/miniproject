cb4c867febd79b21abf3748b0e01670cffbe933c
Hosam Hafez <56209881+HosamHafez@users.noreply.github.com>
2023-11-24 05:06:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,20 +14,18 @@ function displayRandomPlayIdea() {
     const playIdeaElement = document.getElementById('playIdea');
     const playIdeaImage = document.getElementById('playIdeaImage');
     
-    // Randomly select a play idea
     const randomIndex = getRandomIndex(playIdeas.length);
     currentPlayIdeaIndex = randomIndex;
     
-    // Display the play idea text
     playIdeaElement.textContent = playIdeas[randomIndex].name;
     
-    // Display the play idea image
+
     playIdeaImage.src = playIdeas[randomIndex].image;
     playIdeaImage.alt = playIdeas[randomIndex].name;
 }
 
 function getNewPlayIdea() {
-    // Avoid showing the same play idea consecutively
+
     let newIndex;
     do {
         newIndex = getRandomIndex(playIdeas.length);
